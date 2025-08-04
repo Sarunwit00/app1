6afd9ea8443b096ab7e6af5dcc157d4330e00921
Sarunwit00 <a.sarunwit@gmail.com>
2025-08-04 09:54:32 +0700

    error in clasas components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react"
 import { Header, Content, Footer } from "./func-components"
 import Calender from "./class-components"
+import Button from "./class-components"
 
 function App() {
-    return (
-        <div>
-          <Header />
-          <p><center><Calender /></center></p>
-          <Content />
-          <Footer />
-        </div>
-    )
+    return <Button />
 }
 
 export default App
--- src/class-components.js
+++ src/class-components.js
@@ -1,25 +1,15 @@
 import React, { Component } from "react";
 
-export default class Calender extends Component {
-    getData() {
-        const dayName = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์']
-        const monthName = [
-            'มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน',
-            'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'
-        ]
-        const data = new Date();
-        const weekday = dayName[data.getDay()];
-        const day = data.getDate();
-        const month = monthName[data.getMonth()];
-        const year = data.getFullYear() + 543;
-        return `วัน${weekday} ที่ ${day} ${month} ${year}`;
+export default class Button extends Component {
+    showAlert(msg){
+        alert(msg)
+    }
+
+    onClickHandler = () => {
+        this.showAlert('Hello')
     }
 
     render() {
-        return (
-            <div style={{ textAlign: 'center', marginTop: "20px" }}>
-                {this.getData()}
-            </div>
-        )
+        return <button onClick={this.onClickHandler}>OK</button>    
     }
 }
